4b597571e0852c82979ca6e28a9436b1c2191d65
JP Verkamp <me@jverkamp.com>
2021-01-21 11:51:28 -0500

    Revert, there are tbodies this does mess up. Apply 10 second loop to deal with page rebuilds

--- gmail-oldest-first.user.js
+++ gmail-oldest-first.user.js
@@ -1,16 +1,34 @@
 // ==UserScript==
 // @name         GMail Oldest First
 // @namespace    https://blog.jverkamp.com/
-// @version      0.1
+// @version      0.2
 // @description  Display emails in Gmail oldest first (per page)
 // @author       JP Verkamp
 // @match        https://mail.google.com/*
-// @grant        GM_addStyle
+// @grant        none
 // ==/UserScript==
 
-GM_addStyle(`
-tbody {
-    display: flex !important;
-    flex-direction: column-reverse;
-}
-`);
+/* jshint -W097 */
+'use strict';
+
+(function() {
+    let attemptReverse = () => {
+        let reversed = false;
+        document.querySelectorAll('tbody').forEach((el) => {
+            if (el.childElementCount < 10 || el.getAttribute('style')) {
+                return;
+            }
+            
+            el.setAttribute('style', 'display: flex; flex-direction: column-reverse;');
+            reversed = true;
+            console.log('GMail Oldest First: Reverse applied');
+        });
+
+        if (reversed) {
+            setTimeout(attemptReverse, 10000);
+        } else {
+            setTimeout(attemptReverse, 100);
+        }
+    };
+    setTimeout(attemptReverse, 100);
+})();
